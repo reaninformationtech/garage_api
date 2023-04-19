0dd75809c02f96a82405c98f9f99f2aa7b33f2fc
JONCODER <82702646+joincoders@users.noreply.github.com>
2023-04-19 23:47:31 +0700

    delete

--- README.md
+++
@@ -1,9 +0,0 @@
-<p align="center"><a href="https://www.reandialy.com" target="_blank">
-</a></p>
-
-
-
-## About API garage is global API in Cambodia
-
-## License
-Develop by REAN-IT
--- vendor/haruncpi/laravel-user-activity/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::group([
-    'namespace' => '\Haruncpi\LaravelUserActivity\Controllers',
-    'middleware' => config('user-activity.middleware')
-    ], function () {
-    Route::get(config('user-activity.route_path'), 'ActivityController@getIndex');
-    Route::post(config('user-activity.route_path'), 'ActivityController@handlePostRequest');
-});
